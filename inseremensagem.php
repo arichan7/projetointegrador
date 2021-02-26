4c47ca5e6c79fe0997683de84b3b67ba20537a6a
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-02-26 16:59:18 -0300

    Create inseremensagem.php

---
+++ inseremensagem.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "usuario/controle.php";
+
+$controleUsuario = new ControleUsuario();
+
+$login = $controleUsuario->getLogin();
+
+if (!empty($login)) {
+	require_once 'inseremensagemform.html';
+	
+}
+	
+?>
