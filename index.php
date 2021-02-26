029a60943492cd7f0bfa885f2c6860d595f0418f
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-02-26 16:55:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 	<center>
 	<h1><img width="200px" src="imagens/logo.jpeg"  /></h1>
 	<?php require_once 'usuariologado.php'; ?>
+	<?php require_once 'inseremensagem.php'; ?>
+	<br>
+	<?php require_once 'listamensagens.php'; ?>
 	</center>
 	
 </body>
